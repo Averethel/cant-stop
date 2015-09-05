5c524e03142420862c18483c0679739c3c4d14b1
Krzysztof Sakwerda <ksakwerda@gmail.com>
2015-09-05 18:05:02 +0200

    + Winner info in API

--- lib/cant-stop/serializers/games/show.rb
+++ lib/cant-stop/serializers/games/show.rb
@@ -13,7 +13,9 @@ module Serializers::Games
         player_positions: @game.current_positions,
         dice_roll: @game.current_dice_roll,
         dice_pairs: @game.current_dice_pairs_with_validity,
-        current_player: @game.current_player
+        current_player: @game.current_player,
+        game_over: @game.game_over?,
+        winner: @game.winner
       }
     end
   end
