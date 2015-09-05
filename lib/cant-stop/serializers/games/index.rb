10a6cc29b569dca1e0e21e87610675dd048a70d5
Krzysztof Sakwerda <ksakwerda@gmail.com>
2015-09-05 11:08:15 +0200

    + Extract collection serialization

--- lib/cant-stop/serializers/games/index.rb
+++ lib/cant-stop/serializers/games/index.rb
@@ -1,17 +1,13 @@
-require_relative '../serializers'
+require_relative '../base'
 
 module Serializers::Games
-  class Index
+  class Index < Serializers::Base
     def initialize(game)
       @game = game
     end
 
     def serialize
       {id:@game.id}
-    end
-
-    def self.serialize_collection(collection)
-      collection.map{ |e| new(e).serialize }
     end
   end
 end
